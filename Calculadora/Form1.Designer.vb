3497eaeb9d53da7befb9043ba2c68fe52941e6ca
gallicchio11 <agustingallicchio2017@gmail.com>
2024-12-04 09:45:57 -0300

    Agregar archivos de proyecto.

---
+++ Calculadora/Form1.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button4 = New Button()
+        Button7 = New Button()
+        Button0 = New Button()
+        ButtonPunto = New Button()
+        Button8 = New Button()
+        Button5 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button6 = New Button()
+        Button9 = New Button()
+        ButtonIgual = New Button()
+        ButtonMenos = New Button()
+        ButtonDivision = New Button()
+        ButtonMultiplicacion = New Button()
+        ButtonBorrar = New Button()
+        ButtonMas = New Button()
+        TextBoxResultado = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 64)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(70, 50)
+        Button1.TabIndex = 0
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(12, 140)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(70, 50)
+        Button4.TabIndex = 1
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(12, 216)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(70, 50)
+        Button7.TabIndex = 2
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button0
+        ' 
+        Button0.Location = New Point(12, 292)
+        Button0.Name = "Button0"
+        Button0.Size = New Size(70, 50)
+        Button0.TabIndex = 3
+        Button0.Text = "0"
+        Button0.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonPunto
+        ' 
+        ButtonPunto.Location = New Point(88, 292)
+        ButtonPunto.Name = "ButtonPunto"
+        ButtonPunto.Size = New Size(70, 50)
+        ButtonPunto.TabIndex = 4
+        ButtonPunto.Text = "."
+        ButtonPunto.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(88, 216)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(70, 50)
+        Button8.TabIndex = 5
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(88, 140)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(70, 50)
+        Button5.TabIndex = 6
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(88, 64)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(70, 50)
+        Button2.TabIndex = 7
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(164, 64)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(70, 50)
+        Button3.TabIndex = 8
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(164, 140)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(70, 50)
+        Button6.TabIndex = 9
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(164, 216)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(70, 50)
+        Button9.TabIndex = 10
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonIgual
+        ' 
+        ButtonIgual.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        ButtonIgual.Location = New Point(164, 292)
+        ButtonIgual.Name = "ButtonIgual"
+        ButtonIgual.Size = New Size(70, 50)
+        ButtonIgual.TabIndex = 11
+        ButtonIgual.Text = "="
+        ButtonIgual.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonMenos
+        ' 
+        ButtonMenos.Location = New Point(240, 64)
+        ButtonMenos.Name = "ButtonMenos"
+        ButtonMenos.Size = New Size(70, 50)
+        ButtonMenos.TabIndex = 12
+        ButtonMenos.Text = "-"
+        ButtonMenos.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonDivision
+        ' 
+        ButtonDivision.Location = New Point(240, 140)
+        ButtonDivision.Name = "ButtonDivision"
+        ButtonDivision.Size = New Size(70, 50)
+        ButtonDivision.TabIndex = 13
+        ButtonDivision.Text = "/"
+        ButtonDivision.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonMultiplicacion
+        ' 
+        ButtonMultiplicacion.Location = New Point(240, 216)
+        ButtonMultiplicacion.Name = "ButtonMultiplicacion"
+        ButtonMultiplicacion.Size = New Size(70, 50)
+        ButtonMultiplicacion.TabIndex = 14
+        ButtonMultiplicacion.Text = "*"
+        ButtonMultiplicacion.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonBorrar
+        ' 
+        ButtonBorrar.Location = New Point(240, 292)
+        ButtonBorrar.Name = "ButtonBorrar"
+        ButtonBorrar.Size = New Size(70, 50)
+        ButtonBorrar.TabIndex = 15
+        ButtonBorrar.Text = "C"
+        ButtonBorrar.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonMas
+        ' 
+        ButtonMas.Location = New Point(240, 8)
+        ButtonMas.Name = "ButtonMas"
+        ButtonMas.Size = New Size(70, 50)
+        ButtonMas.TabIndex = 16
+        ButtonMas.Text = "+"
+        ButtonMas.UseVisualStyleBackColor = True
+        ' 
+        ' TextBoxResultado
+        ' 
+        TextBoxResultado.Location = New Point(12, 21)
+        TextBoxResultado.Name = "TextBoxResultado"
+        TextBoxResultado.Size = New Size(222, 23)
+        TextBoxResultado.TabIndex = 17
+        TextBoxResultado.Text = "0"
+        TextBoxResultado.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(320, 360)
+        Controls.Add(TextBoxResultado)
+        Controls.Add(ButtonMas)
+        Controls.Add(ButtonBorrar)
+        Controls.Add(ButtonMultiplicacion)
+        Controls.Add(ButtonDivision)
+        Controls.Add(ButtonMenos)
+        Controls.Add(ButtonIgual)
+        Controls.Add(Button9)
+        Controls.Add(Button6)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button5)
+        Controls.Add(Button8)
+        Controls.Add(ButtonPunto)
+        Controls.Add(Button0)
+        Controls.Add(Button7)
+        Controls.Add(Button4)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonPunto As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents ButtonIgual As Button
+    Friend WithEvents ButtonMenos As Button
+    Friend WithEvents ButtonDivision As Button
+    Friend WithEvents ButtonMultiplicacion As Button
+    Friend WithEvents ButtonBorrar As Button
+    Friend WithEvents ButtonMas As Button
+    Friend WithEvents TextBoxResultado As TextBox
+
+End Class
---
+++ Calculadora/Form1.vb
@@ -0,0 +1,134 @@
+﻿Public Class Form1
+
+    Dim Operacion As String
+    Dim ValorResultado As Nullable(Of Double) = Nothing
+    Dim Valor2 As Nullable(Of Double) = Nothing
+    Dim SePresionaOperador As Boolean
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "1"
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "2"
+
+    End Sub
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "3"
+
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "4"
+
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "5"
+
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "6"
+
+    End Sub
+
+    Private Sub Button7_Click_1(sender As Object, e As EventArgs) Handles Button7.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "7"
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "8"
+
+    End Sub
+
+    Private Sub Button9_Click_1(sender As Object, e As EventArgs) Handles Button9.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "9"
+
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextBoxResultado.Text &= "0"
+
+    End Sub
+
+    Private Sub ButtonPunto_Click(sender As Object, e As EventArgs) Handles ButtonPunto.Click
+        EvaluaRestriccionesParaConcatenar()
+        If InStr(TextBoxResultado.Text, ".", CompareMethod.Text) = 0 Then
+            TextBoxResultado.Text &= "."
+        End If
+    End Sub
+
+    Private Sub ButtonIgual_Click(sender As Object, e As EventArgs) Handles ButtonIgual.Click
+        EvaluayHazOperacion()
+        Operacion = ""
+
+    End Sub
+
+    Private Sub ButtonBorrar_Click(sender As Object, e As EventArgs) Handles ButtonBorrar.Click
+        TextBoxResultado.Text = "0"
+        Valor2 = Nothing
+
+
+    End Sub
+
+    Private Sub ButtonMas_Click(sender As Object, e As EventArgs) Handles ButtonMas.Click
+        EvaluayHazOperacion()
+        Operacion = "+"
+
+    End Sub
+
+    Private Sub ButtonMenos_Click(sender As Object, e As EventArgs) Handles ButtonMenos.Click
+        EvaluayHazOperacion()
+        Operacion = "-"
+
+    End Sub
+
+    Private Sub ButtonDivision_Click(sender As Object, e As EventArgs) Handles ButtonDivision.Click
+        EvaluayHazOperacion()
+        Operacion = "/"
+
+    End Sub
+
+    Private Sub ButtonMultiplicacion_Click(sender As Object, e As EventArgs) Handles ButtonMultiplicacion.Click
+        EvaluayHazOperacion()
+        Operacion = "*"
+
+    End Sub
+
+    Public Sub EvaluayHazOperacion()
+        SePresionaOperador = True
+        Valor2 = Val(TextBoxResultado.Text)
+        If ValorResultado IsNot Nothing Then
+            Select Case Operacion
+                Case "+"
+                    ValorResultado = ValorResultado + Valor2
+                Case "-"
+                    ValorResultado = ValorResultado - Valor2
+                Case "*"
+                    ValorResultado = ValorResultado * Valor2
+                Case "/"
+                    ValorResultado = ValorResultado / Valor2
+            End Select
+            TextBoxResultado.Text = ValorResultado
+        Else
+            ValorResultado = Valor2
+        End If
+    End Sub
+
+    Public Sub EvaluaRestriccionesParaConcatenar()
+        If SePresionaOperador = True Then
+            TextBoxResultado.Text = ""
+            SePresionaOperador = False
+        ElseIf TextBoxResultado.Text = "0" Then
+            TextBoxResultado.Text = ""
+        End If
+    End Sub
+End Class
